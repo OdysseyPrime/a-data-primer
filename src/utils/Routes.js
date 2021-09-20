110b724479bcf4651d2f941a504c385df550fae3
VilaLabinot <labinotvila@goprime.io>
2021-09-20 13:29:30 +0200

    Changed scheduler order

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -6,7 +6,143 @@ const routes = [
         id: PAGES.HOME,
     },
     {
-        display: '1. Scheduler',
+        display: '1. Visual Data Hub',
+        id: PAGES.VISUAL_DATA_HUB.ID,
+        children: [
+            {
+                id: PAGES.VISUAL_DATA_HUB.YOUR_FIRST_PIPELINE,
+                display: 'Your First Pipeline',
+            },
+            {
+                display: 'Glob Patterns',
+                id: PAGES.VISUAL_DATA_HUB.GLOB_PATTERNS,
+                children: [
+                    {
+                        display: '1st Example',
+                        id: 'example-1'
+                    },
+                    {
+                        display: '2nd Example',
+                        id: 'example-2'
+                    },
+                    {
+                        display: '3rd Example',
+                        id: 'example-3'
+                    },
+                    {
+                        display: '4th Example',
+                        id: 'example-4'
+                    },
+                    {
+                        display: '5th Example',
+                        id: 'example-5'
+                    },
+                    {
+                        display: '6th Example',
+                        id: 'example-6'
+                    }
+                ]
+            },
+            {
+                display: 'Module JSON configuration',
+                id: PAGES.VISUAL_DATA_HUB.MODULE_JSON_CONFIG
+            },
+            {
+                display: 'Ensuring Data Append',
+                id: PAGES.VISUAL_DATA_HUB.ENSURING_DATA_APPENDING
+            },
+            {
+                display: 'Plain Values Dictionary',
+                id: PAGES.VISUAL_DATA_HUB.PLAIN_VALUES_DICTIONARY,
+                children: [
+                    {
+                        display: 'What is Plain Values Dictionary?',
+                        id: 'what-is-pvd-error'
+                    },
+                    {
+                        display: 'Overcoming the issue',
+                        id: 'overcoming-the-issue'
+                    }
+                ]
+            },
+            {
+                display: 'Querying S3 data on Athena',
+                id: PAGES.VISUAL_DATA_HUB.QUERYING_S3_DATA_ON_ATHENA
+            },
+            {
+                display: 'Processing JSON Data',
+                id: PAGES.VISUAL_DATA_HUB.PROCESSING_JSON_DATA,
+                children: [
+                    {
+                        display: 'Use Case',
+                        id: 'use-case'
+                    },
+                    {
+                        display: 'Solution',
+                        id: 'solution'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        display: '2. Dashboards',
+        id: PAGES.DASHBOARDS.ID,
+        children: [
+            {
+                display: 'Your First Dashboard',
+                id: PAGES.DASHBOARDS.YOUR_FIRST_DASHBOARD,
+                children: [
+                    {
+                        display: 'Exporting to Data Lake',
+                        id: 'exporting-to-data-lake'
+                    },
+                    {
+                        display: 'Dashboard Creation',
+                        id: 'dashboard-creation'
+                    }
+                ]
+            },
+            {
+                display: 'Data Splits and Partitions',
+                id: PAGES.DASHBOARDS.DATA_SPLITS_AND_PARTITIONS,
+                children: [
+                    {
+                        display: 'Overhead Issues',
+                        id: 'overhead-issues'
+                    },
+                    {
+                        id: 'repartition',
+                        display: 'Repartition'
+                    },
+                    {
+                        id: 'partition-by',
+                        display: 'Partition by'
+                    }
+                ]
+            },
+            {
+                display: 'Pipeline vs On The Fly Calculations',
+                id: PAGES.DASHBOARDS.PIPELINE_VS_ON_THE_FLY,
+                children: [
+                    {
+                        display: 'Your Own Field',
+                        id: 'your-own-field'
+                    },
+                    {
+                        display: 'Pipeline Module',
+                        id: 'pipeline-module'
+                    },
+                    {
+                        display: 'Conclusion',
+                        id: 'conclusion'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        display: '3. Scheduler',
         id: PAGES.SCHEDULER.ID,
         children: [
             {
@@ -93,142 +229,6 @@ const routes = [
             }
         ]
     },
-    {
-        display: '2. Visual Data Hub',
-        id: PAGES.VISUAL_DATA_HUB.ID,
-        children: [
-            {
-                id: PAGES.VISUAL_DATA_HUB.YOUR_FIRST_PIPELINE,
-                display: 'Your First Pipeline',
-            },
-            {
-                display: 'Glob Patterns',
-                id: PAGES.VISUAL_DATA_HUB.GLOB_PATTERNS,
-                children: [
-                    {
-                        display: '1st Example',
-                        id: 'example-1'
-                    },
-                    {
-                        display: '2nd Example',
-                        id: 'example-2'
-                    },
-                    {
-                        display: '3rd Example',
-                        id: 'example-3'
-                    },
-                    {
-                        display: '4th Example',
-                        id: 'example-4'
-                    },
-                    {
-                        display: '5th Example',
-                        id: 'example-5'
-                    },
-                    {
-                        display: '6th Example',
-                        id: 'example-6'
-                    }
-                ]
-            },
-            {
-                display: 'Module JSON configuration',
-                id: PAGES.VISUAL_DATA_HUB.MODULE_JSON_CONFIG
-            },
-            {
-                display: 'Ensuring Data Append',
-                id: PAGES.VISUAL_DATA_HUB.ENSURING_DATA_APPENDING
-            },
-            {
-                display: 'Plain Values Dictionary',
-                id: PAGES.VISUAL_DATA_HUB.PLAIN_VALUES_DICTIONARY,
-                children: [
-                    {
-                        display: 'What is Plain Values Dictionary?',
-                        id: 'what-is-pvd-error'
-                    },
-                    {
-                        display: 'Overcoming the issue',
-                        id: 'overcoming-the-issue'
-                    }
-                ]
-            },
-            {
-                display: 'Querying S3 data on Athena',
-                id: PAGES.VISUAL_DATA_HUB.QUERYING_S3_DATA_ON_ATHENA
-            },
-            {
-                display: 'Processing JSON Data',
-                id: PAGES.VISUAL_DATA_HUB.PROCESSING_JSON_DATA,
-                children: [
-                    {
-                        display: 'Use Case',
-                        id: 'use-case'
-                    },
-                    {
-                        display: 'Solution',
-                        id: 'solution'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        display: '3. Dashboards',
-        id: PAGES.DASHBOARDS.ID,
-        children: [
-            {
-                display: 'Your First Dashboard',
-                id: PAGES.DASHBOARDS.YOUR_FIRST_DASHBOARD,
-                children: [
-                    {
-                        display: 'Exporting to Data Lake',
-                        id: 'exporting-to-data-lake'
-                    },
-                    {
-                        display: 'Dashboard Creation',
-                        id: 'dashboard-creation'
-                    }
-                ]
-            },
-            {
-                display: 'Data Splits and Partitions',
-                id: PAGES.DASHBOARDS.DATA_SPLITS_AND_PARTITIONS,
-                children: [
-                    {
-                        display: 'Overhead Issues',
-                        id: 'overhead-issues'
-                    },
-                    {
-                        id: 'repartition',
-                        display: 'Repartition'
-                    },
-                    {
-                        id: 'partition-by',
-                        display: 'Partition by'
-                    }
-                ]
-            },
-            {
-                display: 'Pipeline vs On The Fly Calculations',
-                id: PAGES.DASHBOARDS.PIPELINE_VS_ON_THE_FLY,
-                children: [
-                    {
-                        display: 'Your Own Field',
-                        id: 'your-own-field'
-                    },
-                    {
-                        display: 'Pipeline Module',
-                        id: 'pipeline-module'
-                    },
-                    {
-                        display: 'Conclusion',
-                        id: 'conclusion'
-                    }
-                ]
-            }
-        ]
-    }
 ]
 
 const format = (which) => {
