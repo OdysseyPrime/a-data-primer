e8fe9b86a8f9ae0f45fc76b6e96d5f0e68cf284e
VilaLabinot <labinotvila@goprime.io>
2022-02-23 16:49:46 +0100

    last tweaks on spark calculator

--- src/pages/calculators/SparkConfigurations.js
+++ src/pages/calculators/SparkConfigurations.js
@@ -2,7 +2,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import React, {Fragment, useState} from "react";
 import Typography from "presentations/Typography";
 import Divider from "presentations/Divider";
-import {Button, Checkbox, FormControlLabel, FormGroup, InputAdornment, Table, TextField} from "@material-ui/core";
+import {Button, Checkbox, FormControlLabel, FormGroup, InputAdornment, Link, Table, TextField} from "@material-ui/core";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
@@ -20,17 +20,17 @@ function SparkConfigurations(props) {
 
     const [calculated, setCalculated] = useState(false);
 
-    const initSparkExecutorCores = 5
-    const initEMRCoreInstances = 19
-    const initEMRvCores = 48
-    const initInstanceMemory = 384
+    const initSparkExecutorCores = 3
+    const initEMRCoreInstances = 5
+    const initEMRvCores = 8
+    const initInstanceMemory = 30.5
 
     const [sparkExecutorCores, setSparkExecutorCores] = useState(initSparkExecutorCores);
     const [EMRCoreInstances, setEMRCoreInstances] = useState(initEMRCoreInstances);
     const [EMRvCores, setEMRvCores] = useState(initEMRvCores);
     const [instanceMemory, setInstanceMemory] = useState(initInstanceMemory);
 
-    const [data, setData] = useState({});
+    const [tableData, setTableData] = useState([]);
 
     const updateVariables = () => {
         if (sparkExecutorCores < 1 || EMRCoreInstances < 1 || EMRvCores < 1 || instanceMemory < 1) {
@@ -39,25 +39,30 @@ function SparkConfigurations(props) {
             return;
         }
 
+        let configs = [];
+        configs.push('Total EMR vCores: ' + EMRvCores, 'Available vCores: ' + (EMRvCores - 1))
+
         let expr;
         expr = (EMRvCores - 1) / sparkExecutorCores
         let numberOfExecutorsPerInstance = roundDown ? Math.floor(expr) : Math.ceil(expr);
+        configs.push('Number of executors per node: ' + numberOfExecutorsPerInstance);
 
         expr = instanceMemory / numberOfExecutorsPerInstance
         let totalExecutorMemory = roundDown ? Math.floor(expr) : Math.ceil(expr);
+        configs.push('Total memory per executor: ' + totalExecutorMemory + 'g');
 
         expr = totalExecutorMemory * 0.9
         let sparkExecutorMemory = roundDown ? Math.floor(expr) : Math.ceil(expr);
+        configs.push('Available memory per executor: ' + sparkExecutorMemory + 'g');
 
         let sparkYarnExecutorMemoryOverhead = Math.ceil(totalExecutorMemory * 0.1)
+        configs.push('Overhead memory: ' + sparkYarnExecutorMemoryOverhead + 'g');
+
         let sparkExecutorInstances = (numberOfExecutorsPerInstance * EMRCoreInstances) - 1
         let sparkDefaultParallelism = sparkExecutorInstances * sparkExecutorCores * 2
+        configs.push('Spark default parallelism: ' + sparkDefaultParallelism);
 
-        setData({
-            numberOfExecutorsPerInstance, totalExecutorMemory, sparkExecutorMemory,
-            sparkYarnExecutorMemoryOverhead, sparkExecutorInstances, sparkDefaultParallelism
-        })
-
+        setTableData(configs);
         setCalculated(true);
     }
 
@@ -78,6 +83,11 @@ function SparkConfigurations(props) {
                 order to utilize EMR resources in the best way.
             </Typography>
 
+            <Typography>
+                <Link href="https://aws.amazon.com/blogs/big-data/best-practices-for-successfully-managing-memory-for-apache-spark-applications-on-amazon-emr/">
+                    These calculations are based on official AWS documentation.</Link>
+            </Typography>
+
             <Typography variant={'p'}>
                 Below, we will describe in more details what the input boxes mean:
 
@@ -88,6 +98,8 @@ function SparkConfigurations(props) {
                     <li>Instance memory: memory of one EMR core node.</li>
                     <li>Number of core instances: the total number of EMR core (and task) nodes.</li>
                 </ol>
+
+
             </Typography>
 
             <Typography><TextField label={'Spark Executor Cores'} style={{'width': '30%'}}
@@ -119,43 +131,20 @@ function SparkConfigurations(props) {
 
             <Typography>
                 <FormGroup>
-                    <FormControlLabel control={<Checkbox onChange={handleRoundDown} value={roundDown}/>}
-                                      label="Round down? (Check this if you do not want to allocate more resources)"/>
+                    <FormControlLabel
+                        control={<Checkbox color={'primary'} onChange={handleRoundDown} value={roundDown}/>}
+                        label="Round down? (Less overhead resources)"/>
                 </FormGroup>
             </Typography>
 
             {calculated ?
                 <Table style={{width: '50%'}}>
                     <TableBody>
-                        <TableRow>
-                            <TableCell width={'80%'}>Total executor memory</TableCell>
-                            <TableCell width={'20%'}>{data.totalExecutorMemory}</TableCell>
-                        </TableRow>
-
-                        <TableRow>
-                            <TableCell width={'80%'}>spark.num.executors</TableCell>
-                            <TableCell width={'20%'}>{data.numberOfExecutorsPerInstance}</TableCell>
-                        </TableRow>
-
-                        <TableRow>
-                            <TableCell width={'80%'}>spark.executors.memory</TableCell>
-                            <TableCell width={'20%'}>{data.sparkExecutorMemory}</TableCell>
-                        </TableRow>
-
-                        <TableRow>
-                            <TableCell width={'80%'}>spark.yarn.executor.memoryOverhead</TableCell>
-                            <TableCell width={'20%'}>{data.sparkYarnExecutorMemoryOverhead}</TableCell>
-                        </TableRow>
-
-                        <TableRow>
-                            <TableCell width={'80%'}>spark.executor.instances</TableCell>
-                            <TableCell width={'20%'}>{data.sparkExecutorInstances}</TableCell>
-                        </TableRow>
-
-                        <TableRow>
-                            <TableCell width={'80%'}>spark.default.parallelism</TableCell>
-                            <TableCell width={'20%'}>{data.sparkDefaultParallelism}</TableCell>
-                        </TableRow>
+                        {tableData.map(info => (
+                            <TableRow>
+                                <TableCell width={'100%'}>{info}</TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
                 </Table> : null}
         </Fragment>
